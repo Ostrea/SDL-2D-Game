ffda9d345e4ae6faf2cc41662de8df3748fbd27c
ostrea <ostrea@example.com>
2014-11-09 19:16:55 +0300

    Highscores now properly written to file

--- highscoresscreen.cpp
+++ highscoresscreen.cpp
@@ -4,13 +4,42 @@
 #include <iostream>
 
 void HighScoresScreen::loadContent() {
-    for (int i = 0; i < NUMBER_OF_SCORES; ++i) {
-        highScores.push_back(std::pair<std::string, int>("-", 0));
-    }
+//    for (int i = 0; i < NUMBER_OF_SCORES; ++i) {
+//        highScores.push_back(std::pair<std::string, int>("Ostrea", 0));
+//    }
+//
+//    std::ofstream file("/home/ostrea/Programs/Labs_second_term/"
+//            "Gushin/Coursework_third_try/coursework_third_try/highscores", std::ios::out | std::ios::binary);
 
-    std::ofstream file("/home/ostrea/Programs/Labs_second_term/"
-            "Gushin/Coursework_third_try/coursework_third_try/highscores", std::ios::out | std::ios::binary);
 //    std::copy(highScores.begin(), highScores.end(), std::ostreambuf_iterator<char>(file));
+
+//    size_t sz = myVector.size();
+//    file.write(reinterpret_cast<char*>(&sz), sizeof(sz));
+//    file.write(reinterpret_cast<char*>(&myVector[0]), sz * sizeof(myVector[0]));
+
+//    for (auto item : highScores) {
+//        unsigned long size = item.first.size() + 1;
+//        file.write(reinterpret_cast<char*>(&size), sizeof(size));
+//        auto string = item.first.c_str();
+//        file.write(&string[0], sizeof(char) * size);
+//        int score = item.second;
+//        file.write(reinterpret_cast<char*>(&score), sizeof(score));
+//    }
+//    file.close();
+
+    std::ifstream file2("/home/ostrea/Programs/Labs_second_term/"
+            "Gushin/Coursework_third_try/coursework_third_try/highscores", std::ios::in | std::ios::binary);
+    for (int i = 0; i < NUMBER_OF_SCORES; ++i) {
+        unsigned long size;
+        file2.read(reinterpret_cast<char*>(&size), sizeof(size));
+        char string[size];
+        file2.read(&string[0], sizeof(char) * size);
+        int score;
+        file2.read(reinterpret_cast<char*>(&score), sizeof(score));
+        std::cout << "size = " << size << " score = " << score << " name = " << string << std::endl;
+        highScores.push_back(std::pair<std::string, int>(string, score));
+    }
+    file2.close();
 
     for (auto score : highScores) {
         std::string message = score.first + " : " + std::to_string(score.second);
